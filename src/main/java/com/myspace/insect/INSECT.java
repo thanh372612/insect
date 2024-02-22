8d9a95194637a1b131247e57510d5f6f2729e131
admin <>
2024-02-22 06:57:42 +0000

    {/src/main/java/com/myspace/insect/INSECT.java}

--- src/main/java/com/myspace/insect/INSECT.java
+++ src/main/java/com/myspace/insect/INSECT.java
@@ -6,12 +6,35 @@ package com.myspace.insect;
 
 public class INSECT implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public INSECT() {
-    }
+	@org.kie.api.definition.type.Label(value = "id")
+	private java.lang.String id;
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
 
+	public INSECT() {
+	}
 
+	public java.lang.String getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.String id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public INSECT(java.lang.String id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
